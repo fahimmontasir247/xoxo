750cc722d35b1f9fd66d9806957b45fbb503631e
fahim <mdfahimmontasir03@gmail.com>
2024-05-22 02:09:13 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'XOXO',
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'TUKI XOXO',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.blue,
+          actions: const <Widget>[Icon(Icons.search)],
+        ),
+        body: ElevatedButton(
+          child: const Text('Click Here'),
+          onPressed: () {
+            print('hey_i_am_fahim');
+            
+          },
+        ));
+  }
+}
